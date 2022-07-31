55a3cf4ee2c92b062c8067fbe2f097b0b7c9c015
SeasonedPorks <yanawarood.yk@gmail.com>
2022-07-31 16:18:21 +0700

    5.2

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <template v-for="item in events">
+      <EventCard
+        v-for="event in item.data"
+        :key="event.id"
+        :event="event"
+      ></EventCard>
+    </template>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "../components/EventCard.vue";
+import EventService from "@/service/EventService.js";
+
+export default {
+  name: "HomeView",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    EventService.getEventsPassengers()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+}
+</style>
