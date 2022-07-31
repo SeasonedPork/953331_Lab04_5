a252b5a6987b85f3c7b672e495d30dccf58a8396
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-07-31 14:29:37 +0700

    some test

---
+++ src/services/EventService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  // baseURL: 'http://localhost:3004',
+  //this should use the WebAPI but for as they said the API is not gud so
+  baseURL: "https://api.instantwebtools.net/v1/passenger?page=0&size=10",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  //Added new call
+  getEvents(perPage, page) {
+    return apiClient.get("/events?_limit=" + perPage + "&_page=" + page);
+  },
+  getEvent(id) {
+    return apiClient.get("/events" + id);
+  },
+};
