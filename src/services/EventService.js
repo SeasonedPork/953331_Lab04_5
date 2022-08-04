b1fc6e818d1cb3c2784e77dffb895afd79cd69e1
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-08-04 20:10:23 +0700

    add ;....bruh

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -13,10 +13,10 @@ const apiClient = axios.create({
 
 export default {
   getEvents(perPage, page) {
-    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+    return apiClient.get("/events?_limit=" + perPage + "&_page=" + page);
   },
   //Added new call
   getEvent(id) {
-    return apiClient.get('/events/' + id)
-  }
-}
+    return apiClient.get("/events/" + id);
+  },
+};
